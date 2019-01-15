5b5f0a823c1cadeb006a39e45f1570d8dee1a75a
stefano <s.kouroupis@gmail.com>
2019-01-15 23:38:29 +0000

    add file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import * as http from 'http';
 import * as url from 'url';
+import * as fs from 'fs';
+
 import { Incoming } from './incoming.model';
 
 (async () => {
+    
+    if (!fs.existsSync('queues')) {
+        fs.writeFile('queues', '', (error) => {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+
     const server = await http.createServer(endpoint);
     server.listen(3000);
 })();
@@ -10,11 +21,11 @@ import { Incoming } from './incoming.model';
 function endpoint(req: http.IncomingMessage, res: http.ServerResponse) {
     if (req.url) {
         const queryData = (url.parse(req.url, true).query) as unknown as Incoming;
-        if(queryData.name && queryData.priority && queryData.message){
-            res.end("OK");
+        if (queryData.name && queryData.priority && queryData.message) {
+            res.end('OK');
         } else {
-            res.end("query parameters are not correct");
+            res.end('query parameters are not correct');
         }
     }
-   
+
 }
